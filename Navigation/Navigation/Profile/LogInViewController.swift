707f5a664c7db18f3b0d640947993dc4cefaad40
homromr <solkenth@gmail.com>
2022-04-05 20:51:00 +0300

    Almost done...

--- Navigation/Navigation/Profile/LogInViewController.swift
+++ Navigation/Navigation/Profile/LogInViewController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class LogInViewController: UIViewController {
 
+    private lazy var scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+
+    private lazy var contentView: UIView = {
+        let contentView = UIView()
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        return contentView
+    }()
+
     private lazy var logoImageView: UIImageView = {
         let logo = UIImage(named: "logo")
         let logoImage = UIImageView(image: logo!)
@@ -43,12 +55,12 @@ class LogInViewController: UIViewController {
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.distribution = .fillEqually
-        stackView.spacing = 10
         stackView.addArrangedSubview(loginTextField)
         stackView.addArrangedSubview(passwordTextField)
-        stackView.layer.borderWidth = 0.5 // ?
-        stackView.layer.cornerRadius = 10 // ?
-        //borderWidth: 0.5pt how to ?
+        stackView.layer.borderWidth = 0.5
+        stackView.layer.cornerRadius = 10
+        stackView.layer.borderColor = UIColor.lightGray.cgColor
+        stackView.clipsToBounds = true
         stackView.translatesAutoresizingMaskIntoConstraints = false
         return stackView
     }()
@@ -58,8 +70,10 @@ class LogInViewController: UIViewController {
         button.setTitle("Log In", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .systemGray2
+        let pixel = UIImage(named: "blue_pixel")
+        button.setBackgroundImage(pixel, for: .normal)
         button.layer.cornerRadius = 10
-        //button.backgroundImage(for: .normal) = UIImage(named: "blue_pixel")??
+        button.clipsToBounds = true
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
@@ -73,11 +87,42 @@ class LogInViewController: UIViewController {
     }
 
     private func setupViews() {
-        view.addSubview(logoImageView)
-        view.addSubview(credsStackView)
-        view.addSubview(loginButton)
+        self.view.addSubview(self.scrollView)
+        self.scrollView.addSubview(self.contentView)
+        self.contentView.addSubview(logoImageView)
+        self.contentView.addSubview(credsStackView)
+        self.contentView.addSubview(loginButton)
     }
     private func activateViewConstraints() {
+        let constraints = [
+            scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
 
+            contentView.topAnchor.constraint(equalTo: self.scrollView.topAnchor),
+            //contentView.leadingAnchor.constraint(equalTo: self.scrollView.leadingAnchor),
+            contentView.centerXAnchor.constraint(equalTo: self.scrollView.centerXAnchor),
+            contentView.bottomAnchor.constraint(equalTo: self.scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: self.scrollView.widthAnchor),
+            //contentView.trailingAnchor.constraint(equalTo: self.scrollView.trailingAnchor),
+
+            logoImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 120),
+            logoImageView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 100),
+            logoImageView.heightAnchor.constraint(equalToConstant: 100),
+
+            credsStackView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 120),
+            credsStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            credsStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            credsStackView.heightAnchor.constraint(equalToConstant: 100),
+
+            loginButton.topAnchor.constraint(equalTo: credsStackView.bottomAnchor, constant: 16),
+            loginButton.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            loginButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            loginButton.heightAnchor.constraint(equalToConstant: 50),
+            loginButton.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
     }
 }
