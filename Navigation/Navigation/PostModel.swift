dfff509a4c7b7ada6650139aaca7c156cce9e257
homromr <solkenth@gmail.com>
2022-04-07 21:44:13 +0300

    Ready to create tables, revert if needed

---
+++ Navigation/Navigation/PostModel.swift
@@ -0,0 +1,13 @@
+//
+//  PostFile.swift
+//  Navigation
+//
+//  Created by solkent on 07.04.2022.
+//
+
+import Foundation
+
+struct PostModel {
+    let author, description, image: String
+    var likes, views: Int
+}
